dfc48ea0c15f218b7bf351ff5034b4390a68d96c
cmgdragon <carlos_ole1996@hotmail.com>
2020-10-30 18:58:28 +0100

    changed stylelint config and some minor css changes

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,11 +4,25 @@ module.exports = {
       "stylelint-scss"
     ],
     "rules": {
-      "selector-nested-pattern": "^&",
+      "scss/selector-nest-combinators": "always",
       "indentation": 2,
+      "no-eol-whitespace": true,
       "no-descending-specificity": null,
-      "no-eol-whitespace": null,
-      "declaration-empty-line-before": null,
-      "value-keyword-case": null
+      "no-extra-semicolons": true,
+      "value-keyword-case": "lower",
+      "scss/comment-no-empty": true,
+      "scss/no-dollar-variables": true,
+      "scss/no-duplicate-dollar-variables": true,
+      "scss/no-duplicate-mixins": true,
+      "at-rule-no-unknown": [
+        true,
+        {
+          "ignoreAtRules": [
+            "use",
+            "mixin",
+            "include"
+          ]
+        }
+      ]
     }
 }
